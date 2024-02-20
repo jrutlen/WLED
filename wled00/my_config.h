dfbdcc95f9ecff1278a8d7fb2e7afd5a7a71006d
Jon Rutlen <github@rutlen.net>
2024-02-20 09:46:13 -0500

    Add config for SpectaculEARS

---
+++ wled00/my_config.h
@@ -0,0 +1,36 @@
+#pragma once
+// uncomment to force the compiler to show a warning to confirm that this file is included
+//#warning **** my_config.h: Settings from this file are honored ****
+
+/* Uncomment to use your WIFI settings as defaults
+  //WARNING: this will hardcode these as the default even after a factory reset
+#define CLIENT_SSID "Your_SSID"
+#define CLIENT_PASS "Your_Password"
+*/
+
+//#define MAX_LEDS 1500       // Maximum total LEDs. More than 1500 might create a low memory situation on ESP8266.
+//#define MDNS_NAME "wled"    // mDNS hostname, ie: *.local
+
+#define USERMOD_BATTERY // Enable battery status usermod
+#define USERMOD_BATTERY_MEASUREMENT_PIN 0 // Battery measurement pin, ADC1_CH0
+#define USERMOD_BATTERY_MAX_VOLTAGE 4.2 // Maximum battery voltage
+#define USERMOD_BATTERY_MIN_VOLTAGE 3.1 // Minimum battery voltage
+#define USERMOD_BATTERY_TOTAL_CAPACITY 1100 // Battery capacity in mAh
+#define USERMOD_BATTERY_AUTO_OFF_ENABLED true // Enable auto power off when battery is low
+#define USERMOD_BATTERY_AUTO_OFF_THRESHOLD 3 // Battery percentage threshold for auto power off
+#define USERMOD_BATTERY_LOW_POWER_INDICATOR_ENABLED true // Enable low power indicator
+#define USERMOD_BATTERY_LOW_POWER_INDICATOR_THRESHOLD 5 // Battery percentage threshold for low power indicator
+#define USERMOD_BATTERY_LOW_POWER_INDICATOR_PRESET 1 // Preset to use for low power indicator
+
+#define MDNS_NAME "SpectaculEARS" // mDNS hostname, ie: *.local
+
+#define WLED_MAX_BUTTONS 2 // Maximum number of buttons
+#define LEDPIN 1
+#define DEFAULT_LED_COUNT 88 //SpectaculEARS has 88 LEDs
+#define WLED_AP_SSID "SpectaculEARS" // Name of default WLED access point
+#define WLED_AP_PASS "itmightbemagic" // Password for default WLED access point
+
+#define WLED_VERSION "SE"
+#define BTNPIN 9 
+
+#define SERVERNAME "SpectaculEARS" // Name of WLED server for API calls
